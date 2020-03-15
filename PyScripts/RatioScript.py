3ed79446e466d5d738a1aa1c5cae0b289a5cde5d
mphipps2017 <mnphipps98@gmail.com>
2020-03-15 13:49:11 -0500

    Some formating for 1

--- PyScripts/RatioScript.py
+++ PyScripts/RatioScript.py
@@ -3,6 +3,6 @@ import csv
 #def getRatios(fileDirectory)
     #TODO When completed make it so this runs using the above method header
 with open('PyScripts/HasData.csv') as csvfile:
-reader = csv.reader(csvfile)
-for row in reader:
-    print(row) 
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row) 
