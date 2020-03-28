c8c179e499aaf0876356fa41b74c83397e3d1b7a
mphipps2017 <mnphipps98@gmail.com>
2020-03-28 16:52:14 -0500

    Created a todo

--- PyScripts/Main.py
+++ PyScripts/Main.py
@@ -5,6 +5,7 @@ import os
 #if(fileDirectory == ""):
     #fileDirectory = "Financial_Report.xlsx"
 fileDirectory = os.getcwd() +"/Financial_Report.xlsx"
+#TODO Make this url dynamic (issue a get request and skim page for first occurence of 10-k and the skim page for first occurence ofdata/compname/doc#)
 url ="http://sec.gov/Archives/edgar/data/20639/000114036119005745/Financial_Report.xlsx"
 urllib.request.urlretrieve(url, fileDirectory)
 name = RatioScript.getRatios(fileDirectory)
