cdf9abd73f834b3a52928a61957f17b73c8b6b51
mphipps2017 <mnphipps98@gmail.com>
2020-03-28 17:24:52 -0500

    Now accepts just a ticker wit same output

--- PyScripts/Main.py
+++ PyScripts/Main.py
@@ -1,12 +1,30 @@
 import RatioScript
 import urllib.request
 import os
-#fileDirectory = input("Data Directory:\n")
+ticker = input("CompanyTicker:\n")
 #if(fileDirectory == ""):
     #fileDirectory = "Financial_Report.xlsx"
 fileDirectory = os.getcwd() +"/Financial_Report.xlsx"
-#TODO Make this url dynamic (issue a get request and skim page for first occurence of 10-k and the skim page for first occurence ofdata/compname/doc#)
-url ="http://sec.gov/Archives/edgar/data/20639/000114036119005745/Financial_Report.xlsx"
+htmlUrl = urllib.request.urlopen("https://www.sec.gov/cgi-bin/browse-edgar?CIK="+ticker+"&action=getcompany&owner=exclude")
+htmlDoc = htmlUrl.read().splitlines()
+found = False
+tenK = False
+url = ""
+i = 0
+while (not found) and (i < len(htmlDoc)):
+    if(not tenK and "10-K" in str(htmlDoc[i])):
+        tenK = True
+    elif(tenK and "/Archives/edgar/data/" in str(htmlDoc[i])):
+        url = htmlDoc[i]
+        found = True
+    i = i + 1
+splitUrl = str(url).split('/')
+url = ""
+for i in range(len(splitUrl)):
+    if(splitUrl[i] == "data"):
+        url = splitUrl[i+1] + "/"+splitUrl[i+2]
+        break
+url ="http://sec.gov/Archives/edgar/data/"+url+"/Financial_Report.xlsx"
 urllib.request.urlretrieve(url, fileDirectory)
 name = RatioScript.getRatios(fileDirectory)
 os.remove(fileDirectory)
